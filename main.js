4c443d3125692b5abd89c7834597429fd8384985
KonradWrbl <44879475+KonradWrbl@users.noreply.github.com>
2019-02-28 20:33:22 +0100

    on object

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
-const input = [[0, "H", "cat", "beach", "sun"],
- [1, "V", "selfie", "smile"],
-  [2, "V", "garden", "selfie"],
-   [3, "H", "garden", "cat"]];
+// const input = [[0, "H", "cat", "beach", "sun"],
+//  [1, "V", "selfie", "smile"],
+//   [2, "V", "garden", "selfie"],
+//    [3, "H", "garden", "cat"]];
+
+const input = [{
+        id: 0,
+        tagsumber: 3,
+        tags: ["cat", "beach", "sun"],
+    },
+    {
+        id: 1,
+        tagsumber: 2,
+        tags: ["selfie", "smile"],
+    },
+    {
+        id: 2,
+        tagsumber: 2,
+        tags: ["garden", "selfie"],
+    },
+    {
+        id: 3,
+        tagsumber: 2,
+        tags: ["garden", "cat"],
+    },
+];
 
 let pointsoftree = (firstTab, scndTab) => {
-    let existInFirstTab = firstTab.length-2;
+    let existInFirstTab = firstTab.length;
     let existInBoth = 0;
-    let existInScndTab = scndTab.length-2;
+    let existInScndTab = scndTab.length;
     let smallest;
 
-    for(i=2; i<firstTab.length;i++){
-        for(j=2;j<scndTab.length; j++){
+    for(i=0; i<firstTab.length;i++){
+        for(j=0;j<scndTab.length; j++){
             if(firstTab[i]==scndTab[j]){
                 existInBoth++;
             }
@@ -26,4 +48,4 @@ let pointsoftree = (firstTab, scndTab) => {
     console.log(existInBoth, existInFirstTab, existInScndTab);
     return smallest;
 }
-console.log(pointsoftree(input[0], input[3]));
+console.log(pointsoftree(input[0].tags, input[3].tags));
