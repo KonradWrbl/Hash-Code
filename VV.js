9330c5b26d316c7c1ae00f5b5874a4c996dcc870
KonradWrbl <44879475+KonradWrbl@users.noreply.github.com>
2019-02-28 21:52:34 +0100

    final

---
+++ VV.js
@@ -0,0 +1,111 @@
+const input = [{
+    id: 0,
+    tagsNumber: 3,
+    tags: ["cat", "garden", "sun"],
+},
+{
+    id: 1,
+    tagsNumber: 2,
+    tags: ["selfie", "smile"],
+},
+{
+    id: 2,
+    tagsNumber: 2,
+    tags: ["garden", "selfie"],
+},
+{
+    id: 3,
+    tagsNumber: 2,
+    tags: ["garden", "cat"],
+},
+{
+    id: 4,
+    tagsNumber: 2,
+    tags: ["selfie", "smile"],
+},
+{
+    id: 5,
+    tagsNumber: 2,
+    tags: ["garden", "selfie"],
+},
+{
+    id: 6,
+    tagsNumber: 2,
+    tags: ["garden", "cat"],
+},
+{
+    id: 7,
+    tagsNumber: 2,
+    tags: ["garden", "cat", 'sun'],
+},
+];
+
+var slides = [];
+
+let slideVV = (input) => {
+    let tab = input;
+    for(let x = 1; x<tab.length; tab++){
+        tab.tags[0].length
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+let VV = (input) => {
+    let tab = input;
+    let bestConnection = 0;
+    let i = 0;
+    let x = 0;
+    let connection = 0;
+    while(1) {
+        for(y=1; y<tab.length; y++) {
+            connection = tab[0].tags.length+tab[y].tags.length-pointsoftree(tab[0].tags, tab[y].tags);
+            if(connection ==  bestConnection) {
+                bestConnection = connection;
+                i=y;
+            }else if(connection >  bestConnection) {
+                bestConnection = connection;
+                i=y;
+            }
+        }
+
+        slides[x] = [tab[0], tab[i]];
+        x++;
+
+        tab.splice(0,1);
+        tab.splice(i-1,1);
+
+        console.log(connection);
+
+        if(tab.length==0) break;
+    }
+}
+
+let pointsoftree = (firstTab, scndTab) => {
+    let existInFirstTab = firstTab.length;
+    let existInBoth = 0;
+    let existInScndTab = scndTab.length;
+    let smallest;
+
+    for(i=0; i<firstTab.length;i++){
+        for(j=0;j<scndTab.length; j++){
+            if(firstTab[i]==scndTab[j]){
+                existInBoth++;
+            }
+        }
+    }
+
+    return existInFirstTab+existInScndTab;
+}
+
+VV(input);
+
+console.log(slides);
